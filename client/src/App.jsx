f6f36e9e523a9ac0587fbc8d63b0cb43ef2a918f
codingvivek137 <viveksinghmet@gmail.com>
2025-04-13 22:36:36 +0530

    server setup and socket io setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import EditorPage from "./component/EditorPage.jsx";
 import Home from "./component/Home.jsx";
 import { Routes, Route } from "react-router-dom";
@@ -5,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
+      <Toaster position="top-center"></Toaster>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/editor/:roomId" element={<EditorPage />} />
